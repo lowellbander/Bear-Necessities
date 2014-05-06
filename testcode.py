e89cbf005681bb51e067a1c2cc03c06b49a00fd6
Lowell Bander <Lowell@s-169-232-124-150.resnet.ucla.edu>
2014-05-05 22:27:15 -0700

    adding test code

--- testcode.py
+++ testcode.py
@@ -1,6 +1,15 @@
+def post():
+    url = "http://127.0.0.1:5000/post"
+    post = {}
 
 def main():
     print "hello world"
+
+data = urllib.urlencode(course)
+req = urllib2.Request(url, data)
+response = urllib2.urlopen(req)
+the_page = response.read()
+print the_page
 
 if __name__ == "__main__":
     main()
