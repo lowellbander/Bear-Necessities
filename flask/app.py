e9d6038e39d95caca4885d8220e8a90fdc43f06b
Lowell Bander <Lowell@Lowells-MacBook-Air-2.local>
2014-05-18 15:35:26 -0700

    question template & view

--- flask/app.py
+++ flask/app.py
@@ -1,6 +1,7 @@
 import json
+from pprint import pprint
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import unirest
 
 import settings
@@ -12,6 +13,21 @@ app.debug = True
 def home_page():
     response = unirest.get(settings.API_URL + 'question', headers={'Content-Type':'application/json'}, params={'embedded':'{"post":1}'})
     return render_template('home.html', data=response.body)
+
+@app.route('/question')
+def question():
+    qid = request.args['id']
+    url = settings.API_URL + 'question/' + qid
+    response = unirest.get(url, headers={'Content-Type':'application/json'}, params={'embedded':'{"post":1}'})
+
+    post_url = settings.API_URL + 'post/' + response.body['post']['_id']
+    post = unirest.get(post_url, headers={'Content-Type':'application/json'}, params={'embedded':'{"user":1}'})
+    pprint(post.body)
+    
+    response.body['user'] = post.body['user']
+    pprint(response.body)
+
+    return render_template('question.html', data=response.body)
 
 if __name__ == '__main__':
     app.run()
