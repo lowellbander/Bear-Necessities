f5b917425799e11df6b83e04eb2740970a10f0bb
Steven La <mrstevenla@gmail.com>
2014-05-18 16:13:47 -0700

    base template with some partials

--- flask/app.py
+++ flask/app.py
@@ -1,7 +1,7 @@
 import os
 import json
 
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 from flask.ext.assets import Environment, Bundle
 import unirest
 
@@ -12,12 +12,16 @@ app.debug = True
 
 assets = Environment(app)
 assets.load_path = [
-    os.path.join(os.path.dirname(__file__), 'bower_components'),
     os.path.join(os.path.dirname(__file__), 'less'),
+    os.path.join(os.path.dirname(__file__), 'js'),
+    os.path.join(os.path.dirname(__file__), 'bower_components'),
 ]
 
 css = Bundle('main.less', filters='less', output='gen/main.css', depends='bootstrap/less/*.less')
 assets.register('css', css)
+
+js = Bundle('jquery/dist/jquery.js', 'bootstrap/dist/js/bootstrap.js', filters='rjsmin', output='gen/main.js')
+assets.register('js', js)
 
 @app.route('/')
 def home_page():
