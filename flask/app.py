2481417403d8fb48c2444cf4d2daa2a2613b27be
Steven La <mrstevenla@gmail.com>
2014-05-18 14:52:41 -0700

    basic flask app

--- flask/app.py
+++ flask/app.py
@@ -1,10 +1,17 @@
-from flask import Flask
-app = Flask(__name_)
+import json
+
+from flask import Flask, render_template
+import unirest
+
+import settings
+
+app = Flask(__name__)
+app.debug = True
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def home_page():
+    response = unirest.get(settings.API_URL + 'question', headers={'Content-Type':'application/json'}, params={'embedded':'{"post":1}'})
+    return render_template('home.html', data=response.body)
 
 if __name__ == '__main__':
     app.run()
-
