dc5c50aba7e5e67cd864dbb39b2ef60b7116e812
Lowell Bander <Lowell@Lowells-MacBook-Air-2.local>
2014-05-18 16:09:51 -0700

    added answers

--- flask/app.py
+++ flask/app.py
@@ -9,6 +9,31 @@ import settings
 app = Flask(__name__)
 app.debug = True
 
+#create add user helper function
+
+
+def get_answers(qid):
+    #qid == question._id
+
+    #get all questions
+    response = unirest.get(settings.API_URL + 'answer/', headers={'Content-Type':'application/json'}, params={'embedded':'{"post":1}'})
+    answers = response.body['_items']
+    #print "found %i answers in total" % len(answers)
+
+    #filter for the ones that have qid has their question's id
+    relevant = []
+    for answer in answers:
+        if answer['question'] == qid:
+            relevant.append(answer)
+    #print "found %i relevant answers" % len(relevant)
+    return relevant
+
+@app.route('/test')
+def test():
+    get_answers(request.args['id'])
+    return "testing..."
+
+
 @app.route('/')
 def home_page():
     response = unirest.get(settings.API_URL + 'question', headers={'Content-Type':'application/json'}, params={'embedded':'{"post":1}'})
@@ -20,12 +45,18 @@ def question():
     url = settings.API_URL + 'question/' + qid
     response = unirest.get(url, headers={'Content-Type':'application/json'}, params={'embedded':'{"post":1}'})
 
+    #get the post (parent)
     post_url = settings.API_URL + 'post/' + response.body['post']['_id']
     post = unirest.get(post_url, headers={'Content-Type':'application/json'}, params={'embedded':'{"user":1}'})
     
     response.body['user'] = post.body['user']
     response.body['votes'] = response.body['post']['upvotes'] - response.body['post']['downvotes'] 
-    pprint(response.body)
+    #pprint(response.body)
+
+    #get answers
+    answers = get_answers(qid)
+    pprint(answers)
+    response.body['answers'] = answers
 
     return render_template('question.html', data=response.body)
 
