d2bdacbec0eef9c864af98f53f20605fead8a5ff
Steven La <mrstevenla@gmail.com>
2014-05-18 17:59:38 -0700

    fixed api location

--- api/wsgi.py
+++ api/wsgi.py
@@ -1,9 +1,9 @@
 import sys
 sys.path.insert(0, '/simul8/env/bear/lib/python2.7/site-packages')
-sys.path.insert(0, '/simul8/Bear-Necessities/')
+sys.path.insert(0, '/simul8/Bear-Necessities/api')
 
 print sys.path
 
 from run import make_app
-application = make_app(settings='/simul8/Bear-Necessities/settings.py')
+application = make_app(settings='/simul8/Bear-Necessities/api/settings.py')
 
