c261f07a6df64e85fdb6c287077f9af6b88b8121
Steven La <mrstevenla@gmail.com>
2014-05-18 20:02:46 -0700

    ported entire api

--- api/run.py
+++ api/run.py
@@ -7,6 +7,7 @@ from flask.ext.mongorest import operators as ops
 from flask.ext.mongorest import methods  
 
 app = Flask(__name__)
+app.debug = True
 
 app.config.update(
     MONGODB_HOST = 'localhost',
@@ -17,19 +18,20 @@ app.config.update(
 db = MongoEngine(app)
 api = MongoRest(app)
 
+class User(db.Document):
+    name = db.StringField(default='NO NAME')
+    major = db.ListField(db.StringField())
+    minor = db.ListField(db.StringField())
+    reputation = db.IntField(default=0)
+
 class Post(db.Document):
     body = db.StringField(default='This post has no body.')
     upvotes = db.IntField(default=0)
     downvotes = db.IntField(default=0)
     views = db.IntField(default=0)
     active = db.BooleanField(default=True)
-    user = db.EmbeddedDocumentField(User)
-
-class User(db.EmbeddedDocument):
-    name = db.StringField(default='NO NAME')
-    major = db.ListField(db.StringField())
-    minor = db.ListField(db.StringField())
-    reputation = db.IntField(default=0)
+    user = db.ReferenceField(User)
+    meta = {'allow_inheritance': True}
 
 class Question(Post):
     title = db.StringField(default='This question has no title.')
@@ -41,19 +43,57 @@ class Answer(Post):
     question = db.ReferenceField(Question)
 
 class Comment(Post):
-    parent = db.ReferenceField(Comment)
+    parent = db.ReferenceField(Post)
 
+
+class UserResource(Resource):
+    document = User
+    filters = {
+        'name': [ops.Exact, ops.Contains]
+    }
+
+class AnswerResource(Resource):
+    document = Answer
+    filters = {
+        'question': [ops.Exact]
+    }
+
+class CommentResource(Resource):
+    document = Comment
+    filters = {
+        'parent': [ops.Exact]
+    }
 
 class QuestionResource(Resource):
     document = Question
-    filters = {
-        'tag': [ops.Exact, ops.Icontains],
-        'course': [ops.Exact, ops.Icontains],
-        'major': [ops.Exact, ops.Icontains],
+    related_resources = {
+        'user': UserResource
     }
+    filters = {
+        'tag': [ops.Exact, ops.Contains],
+        'course': [ops.Exact, ops.Contains],
+        'major': [ops.Exact, ops.Contains],
+    }
+
+@api.register(name='user', url='/user/')
+class UserView(ResourceView):
+    resource = UserResource
+    methods = [methods.Create, methods.Update, methods.Fetch, methods.List, methods.Delete]
 
 @api.register(name='question', url='/question/')
 class QuestionView(ResourceView):
     resource = QuestionResource
     methods = [methods.Create, methods.Update, methods.Fetch, methods.List, methods.Delete]
-    
+
+@api.register(name='answer', url='/answer/')
+class AnswerView(ResourceView):
+    resource = AnswerResource
+    methods = [methods.Create, methods.Update, methods.Fetch, methods.List, methods.Delete]
+
+@api.register(name='comment', url='/comment/')
+class CommentView(ResourceView):
+    resource = CommentResource
+    methods = [methods.Create, methods.Update, methods.Fetch, methods.List, methods.Delete]
+
+if __name__ == '__main__':
+    app.run()
