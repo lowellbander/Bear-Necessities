3fa5347bba638f0583ef8ea0f47aecb720db2d4b
Lowell Bander <Lowell@Lowells-MacBook-Air-2.local>
2014-05-18 18:40:50 -0700

    now runs in background

--- api/answer_count.py
+++ api/answer_count.py
@@ -1,7 +1,9 @@
 import unirest
 import settings
-from pprint import pprint
+import threading
 
+SYNC_INTERVAL = 43200 # 43200 sec = 12 hrs
+    
 def get_all_questions():
     response = unirest.get(settings.API_URL + 'question/', headers={'Content-Type':'application/json'})
     questions = response.body['_items']
@@ -15,18 +17,16 @@ def get_all_answers():
 def set_nAnswers(qid, nAnswers):
     response = unirest.get(settings.API_URL + 'question/' + qid, headers={'Content-Type':'application/json'})
     datum = {"answers": str(nAnswers)}
-    pprint(response.body)
     etag = response.body['_etag']
 
     response2 = unirest.patch(settings.API_URL +
             'question/' + qid,
             headers={'Accept':'application/json', 'If-Match': etag}, params=datum)
-    pprint(response2.body)
 
-def main():
-
+def syncer():
     questions = get_all_questions()
     answers = get_all_answers()
+
     # for each question, determine how many answers it has, set this as attr
     for question in questions:
         numAnswers = 0
@@ -35,6 +35,10 @@ def main():
                 numAnswers += 1
         if numAnswers is not int(question['answers']):
             set_nAnswers(question['_id'], numAnswers)
+
+def main():
+    t = threading.Timer(SYNC_INTERVAL, syncer)
+    t.start()
 
 if __name__ == '__main__':
     main()
