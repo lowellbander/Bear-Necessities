2a1f3a75bea076cd1c2c5af1529f8bf1c3ff35cb
Steven La <mrstevenla@gmail.com>
2014-05-18 19:17:38 -0700

    added questio nasking

--- flask/app.py
+++ flask/app.py
@@ -2,7 +2,7 @@ import os
 import json
 from pprint import pprint
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from flask.ext.assets import Environment, Bundle
 import unirest
 
@@ -21,7 +21,7 @@ assets.load_path = [
 css = Bundle('main.less', filters='less', output='gen/main.css', depends='bootstrap/less/*.less')
 assets.register('css', css)
 
-js = Bundle('jquery/jquery.js', 'bootstrap/js/bootstrap.js', filters='rjsmin', output='gen/main.js')
+js = Bundle('jquery/jquery.js', 'jquery/jquery.optionTree.js', 'bootstrap/js/bootstrap.js', filters='rjsmin', output='gen/main.js')
 assets.register('js', js)
 
 #create add user helper function
@@ -54,8 +54,33 @@ def test():
 
 @app.route('/')
 def home_page():
-    response = unirest.get(settings.API_URL + 'question', headers={'Content-Type':'application/json'}, params={'embedded':'{"post":1}'})
+    response = unirest.get(settings.API_URL + 'question', headers={'Accept':'application/json'}, params={'embedded':'{"post":1}'})
     return render_template('home.html', data=response.body)
+
+@app.route('/question/ask', methods=['GET', 'POST'])
+def question_ask():
+    courses = {
+        'Computer Science': {
+            '31': 'cs31',
+            '131': 'cs131'
+        }
+    }
+    if request.method == 'POST':
+        print 'wat'
+        print request.form['body']
+        post_response = unirest.post(settings.API_URL + 'post/', headers={'Accept':'application/json'}, params={
+            'body': request.form['body'],
+            'user': '5377df73dae5f13c42854c4e'  # TODO: make this the logged in user
+        })
+        print post_response
+
+        question_response = unirest.post(settings.API_URL + 'question/', headers={'Accept': 'application/json'}, params={
+            'title': request.form['title'],
+            'post': post_response.body['_id']
+        })
+        return redirect(url_for('question', qid=question_response.body['_id']))
+    else:
+        return render_template('question_ask.html', courses=json.dumps(courses))
 
 @app.route('/question/<qid>')
 def question(qid):
