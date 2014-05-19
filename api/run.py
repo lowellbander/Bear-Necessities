11ef74255f35a60b5e3ba223891b2cc4d7d7bead
Steven La <mrstevenla@gmail.com>
2014-05-18 19:40:39 -0700

    changed to mongorest

--- api/run.py
+++ api/run.py
@@ -1,14 +1,59 @@
-import os
+from flask import Flask
+from flask.ext.mongoengine import MongoEngine
+from flask.ext.mongorest import MongoRest
+from flask.ext.mongorest.views import ResourceView
+from flask.ext.mongorest.resources import Resource
+from flask.ext.mongorest import operators as ops
+from flask.ext.mongorest import methods  
 
-from eve import Eve
+app = Flask(__name__)
 
-def make_app(*args, **kwargs):
-    '''
-    Factory for making an Eve application.
-    This is needed to ensure that wsgi.py and run.py have the same methods for
-    instantiating the Eve application
-    '''
-    return Eve(*args, **kwargs)
+app.config.update(
+    MONGODB_HOST = 'localhost',
+    MONGODB_PORT = '27017',
+    MONGODB_DB = 'jungle_book',
+)
 
-if __name__ == '__main__':
-    make_app().run(host='0.0.0.0')
+db = MongoEngine(app)
+api = MongoRest(app)
+
+class Post(db.Document):
+    body = db.StringField(default='This post has no body.')
+    upvotes = db.IntField(default=0)
+    downvotes = db.IntField(default=0)
+    views = db.IntField(default=0)
+    active = db.BooleanField(default=True)
+    user = db.EmbeddedDocumentField(User)
+
+class User(db.EmbeddedDocument):
+    name = db.StringField(default='NO NAME')
+    major = db.ListField(db.StringField())
+    minor = db.ListField(db.StringField())
+    reputation = db.IntField(default=0)
+
+class Question(Post):
+    title = db.StringField(default='This question has no title.')
+    tags = db.ListField(db.StringField())
+    course = db.StringField()
+    major = db.StringField()
+
+class Answer(Post):
+    question = db.ReferenceField(Question)
+
+class Comment(Post):
+    parent = db.ReferenceField(Comment)
+
+
+class QuestionResource(Resource):
+    document = Question
+    filters = {
+        'tag': [ops.Exact, ops.Icontains],
+        'course': [ops.Exact, ops.Icontains],
+        'major': [ops.Exact, ops.Icontains],
+    }
+
+@api.register(name='question', url='/question/')
+class QuestionView(ResourceView):
+    resource = QuestionResource
+    methods = [methods.Create, methods.Update, methods.Fetch, methods.List, methods.Delete]
+    
