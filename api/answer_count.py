309d357afb1665f6cc4c49206546fa214837f6ba
Lowell Bander <Lowell@Lowells-MacBook-Air-2.local>
2014-05-18 18:10:55 -0700

    more code

--- api/answer_count.py
+++ api/answer_count.py
@@ -13,21 +13,28 @@ def get_all_answers():
     return answers
 
 def main():
-    response = unirest.get(settings.API_URL + '/post/536c3df3dae5f13314f7bb65', headers={'Content-Type':'application/json'})
-    pprint(response.body)
+    # response = unirest.get(settings.API_URL + '/post/536c3df3dae5f13314f7bb65', headers={'Content-Type':'application/json'})
+    # pprint(response.body)
 
-    datum = {"views": 22}
-    etag = response.body['_etag']
+    # datum = {"views": "22"}
+    # etag = response.body['_etag']
 
-    response2 = unirest.patch(settings.API_URL +
-            '/post/536c3df3dae5f13314f7bb65',
-            headers={'Accept':'application/json', 'If-Match': etag}, params=datum)
-    pprint(response2.body)
+    # response2 = unirest.patch(settings.API_URL +
+    #         '/post/536c3df3dae5f13314f7bb65',
+    #         headers={'Accept':'application/json', 'If-Match': etag}, params=datum)
+    # pprint(response2.body)
 
-    #questions = get_all_questions()
-    #answers = get_all_answers()
 
+
+    questions = get_all_questions()
+    answers = get_all_answers()
     # for each question, determine how many answers it has, set this as attr
+    for question in questions:
+        numAnswers = 0
+        for answer in answers:
+            if answer['question'] == question['_id']:
+                numAnswers += 1
+        #if numAnswers is not 
 
 
 if __name__ == '__main__':
