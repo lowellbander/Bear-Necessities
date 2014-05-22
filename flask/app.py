6c2d990b1586fda0014f740ec30b7a38524c893d
Lowell Bander <lowellbander@gmail.com>
2014-05-21 21:14:48 -0700

    not a change

--- flask/app.py
+++ flask/app.py
@@ -91,10 +91,10 @@ def question(qid):
     post_url = settings.API_URL + 'post/' + response.body['post']['_id']
     post = unirest.get(post_url, headers={'Content-Type':'application/json'}, params={'embedded':'{"user":1}'})
     print post.code
-    
+
     response.body['post']['user'] = post.body['user'] if post.code == 200 else {}
 
-    response.body['votes'] = response.body['post']['upvotes'] - response.body['post']['downvotes'] 
+    response.body['votes'] = response.body['post']['upvotes'] - response.body['post']['downvotes']
     #pprint(response.body)
 
     #get answers
