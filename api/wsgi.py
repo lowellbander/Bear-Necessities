1149f60ce5679f9bc834c81cbd7bac31523f6b5d
Steven La <mrstevenla@gmail.com>
2014-05-29 14:40:49 -0700

    fixed wsgi to work with new api, changed requirements

--- api/wsgi.py
+++ api/wsgi.py
@@ -2,8 +2,5 @@ import sys
 sys.path.insert(0, '/simul8/env/bear/lib/python2.7/site-packages')
 sys.path.insert(0, '/simul8/Bear-Necessities/api')
 
-print sys.path
-
-from run import make_app
-application = make_app(settings='/simul8/Bear-Necessities/api/settings.py')
+from run import app as application
 
